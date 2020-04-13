39efb4ea3fbba9de195fe168a56fcf53d7414201
Lin, Yue <lin.3326@osu.edu>
2020-04-13 19:42:08 -0400

    update cokriging

--- data-agg-cokriging.R
+++ data-agg-cokriging.R
@@ -2,52 +2,68 @@
 library(sp)
 library(gstat)
 library(rgdal)
+library(lattice)
+library(raster)
+
+# primary data
+data_tomtom <- read.csv("data/d04_tomtom.csv", T)
+class(data_tomtom)
+coordinates(data_tomtom) = ~x+y
+proj4string(data_tomtom) <- CRS("+init=epsg:7131")
+minx_tomtom <- min(data_tomtom$x)
+miny_tomtom <- min(data_tomtom$y)
+
+# secondary data
+data_pems <- read.csv("data/d04_pems.csv", T)
+class(data_pems)
+data_pems <- data_pems[!is.na(data_pems$Avg_Speed),]
+coordinates(data_pems) = ~x+y
+proj4string(data_pems) <- CRS("+init=epsg:7131")
+minx_pems <- min(data_pems$x)
+miny_pems <- min(data_pems$y)
+
+# Read grid
+st_grid <- raster("data/d04_ras1.tif")
+st_grid <- as(st_grid, "SpatialPixels")
+projection(st_grid) <- CRS("+init=epsg:7131")
+class(st_grid)
+
+# cokriging of two variables
+traffic.g <- gstat(id = "tomtom", formula=currentSpeed~1, data=data_tomtom, nmax=10, set=list(nocheck = 1))
+traffic.g <- gstat(traffic.g, "pems", Avg_Speed~1, data_pems, nmax = 10, set=list(nocheck = 1))
+traffic.g <- gstat(traffic.g, model = vgm(1, "Sph", 900, 1), fill.all=T, set = list(nocheck = 1))
+x <- variogram(traffic.g, cutoff=1000)
+traffic.fit = fit.lmc(x, traffic.g, fit.method = 2)
+plot(x, model = traffic.fit)
+z <- predict(meuse.fit, newdata = meuse.grid)
+
+
 
 data(meuse)
-meuse
-class(meuse)
 coordinates(meuse) = ~x+y
-
-proj4string(meuse) = CRS("+init=epsg:28992")
-meuse.ll = spTransform(meuse, CRS("+proj=longlat +datum=WGS84"))
-
 data(meuse.grid)
 gridded(meuse.grid) = ~x+y
+xyplot(y~x, meuse.grid, asp="iso", pch="+")
 
 # cokriging of the four heavy metal variables
 meuse.g <- gstat(id = "zn", formula=log(zinc)~1, data=meuse, nmax=10, set=list(nocheck = 1))
 meuse.g <- gstat(meuse.g, "cu", log(copper)~1, meuse, nmax = 10, set=list(nocheck = 1))
-meuse.g <- gstat(meuse.g, "cd", log(cadmium)~1, meuse, nmax = 10, set = list(nocheck = 1))
-meuse.g <- gstat(meuse.g, "pb", log(lead)~1, meuse, nmax = 10, set = list(nocheck = 1))
 meuse.g <- gstat(meuse.g, model = vgm(1, "Sph", 900, 1), fill.all=T, set = list(nocheck = 1))
 x <- variogram(meuse.g, cutoff=1000)
 meuse.fit = fit.lmc(x, meuse.g)
 plot(x, model = meuse.fit)
 z <- predict(meuse.fit, newdata = meuse.grid)
 
-library(lattice)
-
 pl1 <- spplot(z["zn.pred"], main="log-zinc predictions")
 pl2 <- spplot(z["cu.pred"], main="log-copper predictions")
-pl3 <- spplot(z["cd.pred"], main="log-cadmium predictions")
-pl4 <- spplot(z["pb.pred"], main="log-lead predictions")
 print(pl1, split = c(1,1,2,2), more=TRUE)
-print(pl2, split = c(1,2,2,2), more=TRUE)
-print(pl3, split = c(2,1,2,2), more=TRUE)
-print(pl4, split = c(2,2,2,2))
+print(pl2, split = c(1,2,2,2))
 z$zn.se = sqrt(z$zn.var)
 z$cu.se = sqrt(z$cu.var)
-z$pb.se = sqrt(z$pb.var)
-z$cd.se = sqrt(z$cd.var)
 pl1 <- spplot(z["zn.se"], main="log-zinc std.err.")
 pl2 <- spplot(z["cu.se"], main="log-copper std.err.")
-pl3 <- spplot(z["cd.se"], main="log-cadmium std.err.")
-pl4 <- spplot(z["pb.se"], main="log-lead st.err.")
 print(pl1, split = c(1,1,2,2), more=TRUE)
-print(pl2, split = c(1,2,2,2), more=TRUE)
-print(pl3, split = c(2,1,2,2), more=TRUE)
-print(pl4, split = c(2,2,2,2))
-
+print(pl2, split = c(1,2,2,2))
 rm(meuse.g, x, meuse.fit, z)
 
 # indicator cokriging for the 9 percentiles of zinc:
@@ -69,3 +85,4 @@ plot(x, model = meuse.fit)
 z <- predict(meuse.fit, newdata = meuse.grid)
 spplot(z, c(3,5,7,9,11,13,15,17,19), 
 	names.attr = paste("est.Pr(Zn < ", q, ")", sep = ""))
+
